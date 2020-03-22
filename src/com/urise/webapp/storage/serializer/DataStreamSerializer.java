467665166bb9464e1289ec000e838719e9b75d37
MarusyaAl <marina.artpeople@gmail.com>
2020-03-22 14:41:05 +0300

    HW10: private <T> void writeCollection(Collection<T> collection, DataOutputStream dataOutputStream, elementWriter<T> writer);

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -1,117 +1,106 @@
 package com.urise.webapp.storage.serializer;
 
-import com.google.gson.internal.bind.util.ISO8601Utils;
 import com.urise.webapp.model.*;
 
-import java.beans.Customizer;
 import java.io.*;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.*;
-import java.util.function.Consumer;
+
 
 public class DataStreamSerializer implements StreamSerializerStrategy {
 
     @Override
     public void doWrite(Resume resume, OutputStream outputStream) throws IOException {
         try (DataOutputStream dataOutputStream = new DataOutputStream(outputStream)) {
-
-
             dataOutputStream.writeUTF(resume.getUuid());
             dataOutputStream.writeUTF(resume.getFullName());
             Map<ContactType, String> contacts = resume.getContacts();
-            dataOutputStream.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                dataOutputStream.writeUTF(entry.getKey().name());
-                dataOutputStream.writeUTF(entry.getValue());
-            }
 
+            writeCollection(contacts.entrySet(), dataOutputStream, new elementWriter<Map.Entry<ContactType, String>>() {
+                @Override
+                public void write(Map.Entry<ContactType, String> contactTypeStringEntry) throws IOException {
+                    dataOutputStream.writeUTF(contactTypeStringEntry.getKey().name());
+                    dataOutputStream.writeUTF(contactTypeStringEntry.getValue());
+                }
+            });
             Map<SectionType, Section> sections = resume.getSections();
 
-            dataOutputStream.writeInt(sections.size());   // 1. пишем размер мапы Map<SectionType, Section> sections (для read)
+            writeCollection(sections.entrySet(), dataOutputStream, new elementWriter<Map.Entry<SectionType, Section>>() {
+                @Override
+                public void write(Map.Entry<SectionType, Section> sectionTypeSectionEntry) throws IOException {
+                    //      for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {  // начало основного цикла, проходим по всем эл-там мапы Map<SectionType, Secton> sections
+                    SectionType key = sectionTypeSectionEntry.getKey();                 // определяем значение Key, т.е. Section Type
+                    Section value = sectionTypeSectionEntry.getValue();                 // определяем value, т.е. в самом сложном случае лист Organization Sections
 
-            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {  // начало основного цикла, проходим по всем эл-там мапы Map<SectionType, Secton> sections
+                    dataOutputStream.writeUTF(key.name());  // пишем key, имя этой секции, например EDUCATION = "Образование"
 
-                SectionType key = entry.getKey();                 // определяем значение Key, т.е. Section Type
-                Section value = entry.getValue();                 // определяем value, т.е. в самом сложном случае лист Organization Sections
+                    switch (key) {
+                        case PERSONAL:                                              // случай с value = TextSection
+                        case OBJECTIVE: {
 
-                dataOutputStream.writeUTF(key.name());  // пишем key, имя этой секции, например EDUCATION = "Образование"
-
-                switch (key) {
-                    case PERSONAL:                                              // случай с value = TextSection
-                    case OBJECTIVE: {
-
-                        dataOutputStream.writeUTF(((TextSection) value).getContent());             // 3.1 пишем значение value
-                    }
-                    break;
-
-                    case ACHIEVEMENT:                                            // случай с value = ListSection
-                    case QUALIFICATIONS: {
-
-                        List<String> items = ((ListSection) value).getItems();
-                        int itemSize = items.size();
-                        dataOutputStream.writeInt(itemSize);            // 2.3 пишем значение size для List для read
-                        for (String item : items) {
-                            dataOutputStream.writeUTF(item);                     // 3.2 пишем значение value
+                            dataOutputStream.writeUTF(((TextSection) value).getContent());             // 3.1 пишем значение value
                         }
-                    }
-                    break;
+                        break;
 
-                    case EXPERIENCE:                                   // самый сложный случай с value = OrganizationSection
-                    case EDUCATION: {
-                        // т.е. value = List<Organization> organizations
-                        List<Organization> organizations = ((OrganizationSection) value).getOrganizations(); // получаем лист, где содержатся элементы: класс Organization
+                        case ACHIEVEMENT:                                            // случай с value = ListSection
+                        case QUALIFICATIONS: {
+
+                            List<String> items = ((ListSection) value).getItems();
+                            writeCollection(items, dataOutputStream, new elementWriter<String>() {
+                                @Override
+                                public void write(String s) throws IOException {
+                                    dataOutputStream.writeUTF(s);
+                                }
+                            });
+                        }
+                        break;
+
+                        case EXPERIENCE:                                   // самый сложный случай с value = OrganizationSection
+                        case EDUCATION: {
+                            // т.е. value = List<Organization> organizations
+                            List<Organization> organizations = ((OrganizationSection) value).getOrganizations(); // получаем лист, где содержатся элементы: класс Organization
+
+                            writeCollection(organizations, dataOutputStream, new elementWriter<Organization>() {
+                                @Override
+                                public void write(Organization organization) throws IOException {
+
+                                    Link homepage = organization.getHomePage();                         // получаем элементы этого класса, Link и лист <Position>
+                                    List<Organization.Position> positions = organization.getPositions();    // получаем лист, где содержатся элементы: класс Organization
+                                    String name = homepage.getName();
+                                    String url = homepage.getUrl();
+                                    dataOutputStream.writeUTF(name);                 // 3.4 пишем name из Link
+                                    writeCheckNullString(dataOutputStream, url);    // 3.5 пишем url
 
 
-                        int sizeOrganizations = organizations.size();          // получаем размер этого листа (класс Organization) для read
-                        dataOutputStream.writeInt(sizeOrganizations);         // 3.3 пишем размер этого листа (класс Organization) для read
+                                    writeCollection(positions, dataOutputStream, new elementWriter<Organization.Position>() {
+                                        @Override
+                                        public void write(Organization.Position position) throws IOException {
 
-                        for (Organization organization : organizations) {   //  для каждого элемента List<Organization> organizations
+                                            int startYear = position.getStartDate().getYear();
+                                            int startMonth = position.getStartDate().getMonthValue();
+                                            int endYear = position.getEndDate().getYear();
+                                            int endMonth = position.getEndDate().getMonthValue();
+                                            String title = position.getTitle();
+                                            String description = position.getDescription();
 
-                            Link homepage = organization.getHomePage();                         // получаем элементы этого класса, Link и лист <Position>
-                            List<Organization.Position> positions = organization.getPositions();    // получаем лист, где содержатся элементы: класс Organization
-
-                            String name = homepage.getName();
-
-                            String url = homepage.getUrl();
-
-                            dataOutputStream.writeUTF(name);                 // 3.4 пишем name из Link
-
-                            writeCheckNullString(outputStream, url);    // 3.5 пишем url
-
-                            // для каждого элемента List <Position> positions
-                            int positionsSize = positions.size();
-
-                            dataOutputStream.writeInt(positionsSize);     //3.6 пишем размер листа Positions
-
-                            for (Organization.Position position : positions) {           // конструктор public Position(LocalDate startDate, LocalDate endDate, String title, String description)
-
-                                int startYear = position.getStartDate().getYear();
-                                int startMonth = position.getStartDate().getMonthValue();
-                                int endYear = position.getEndDate().getYear();
-                                int endMonth = position.getEndDate().getMonthValue();
-                                String title = position.getTitle();
-                                String description = position.getDescription();
-
-                                writeDate(outputStream, startYear, startMonth);    // 3.7 пишем startYear   // 3.8 пишем startMonth
-
-                                writeDate(outputStream, endYear, endMonth);   // 3.9 пишем endYear   // 3.10 пишем endMonth
-
-                                writeCheckNullString(outputStream, title);   // 3.11 пишем title
-
-                                writeCheckNullString(outputStream, description);   // 3.12 пишем desctiption
-
-                            }
+                                            writeDate(dataOutputStream, startYear, startMonth);    // 3.7 пишем startYear   // 3.8 пишем startMonth
+                                            writeDate(dataOutputStream, endYear, endMonth);   // 3.9 пишем endYear   // 3.10 пишем endMonth
+                                            writeCheckNullString(dataOutputStream, title);   // 3.11 пишем title
+                                            writeCheckNullString(dataOutputStream, description);   // 3.12 пишем desctiption
+                                        }
+                                    });
+                                }
+                            });
                         }
                     }
                 }
-            }
+            });
         }
     }
 
-    private void writeCheckNullString(OutputStream outputStream, String str) throws IOException {
-        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+    private void writeCheckNullString(DataOutputStream dataOutputStream, String str) throws IOException {
+        //  DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
         if (str != null) {
             dataOutputStream.writeUTF(str);  // 3.12 пишем description
         } else {
@@ -119,24 +108,21 @@ public class DataStreamSerializer implements StreamSerializerStrategy {
         }
     }
 
-    private void writeDate(OutputStream outputStream, int Year, int Month) throws IOException {
-        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+    private void writeDate(DataOutputStream dataOutputStream, int Year, int Month) throws IOException {
+        //   DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
         dataOutputStream.writeInt(Year);
         dataOutputStream.writeInt(Month);
         dataOutputStream.writeInt(1);
     }
 
-    @FunctionalInterface
-    interface myCustomer extends Consumer {
-        default void accept() throws Exception {
-        }
+    interface elementWriter<T> {
+        void write(T t) throws IOException;
     }
 
-    public void writeWithException(Collection collection, myCustomer action, OutputStream outputStream) throws IOException {
-        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-        Objects.requireNonNull(action);
-        for (Object element : collection) {
-            dataOutputStream.writeInt((Integer) element);
+    private <T> void writeCollection(Collection<T> collection, DataOutputStream dataOutputStream, elementWriter<T> writer) throws IOException {
+        dataOutputStream.writeInt(collection.size());
+        for (T element : collection) {
+            writer.write(element);
         }
     }
 
@@ -170,7 +156,6 @@ public class DataStreamSerializer implements StreamSerializerStrategy {
                             items.add(dataInputStream.readUTF());     // 3.2 читаем значение value
                         }
                         resume.addSection(SectionType.valueOf(keyName), new ListSection(items));    // 3.2 читаем значение value
-                        //   resume.addSection(SectionType.valueOf(keyName), new ListSection(dataInputStream.readUTF()));
 
                         break;
                     case "EXPERIENCE":
@@ -201,7 +186,6 @@ public class DataStreamSerializer implements StreamSerializerStrategy {
 
                                 positions.add(position);
                             }
-                            // для каждого элемента Position листа positions закрываем, лист positions собран
                             Organization organization = new Organization(homepage, positions);
                             organizations.add(organization);     //  лист organizatins <Organization> собран
                         }
